5e32b46668c07f97b0764813977b24a7f4ba6c5c
mimansa59 <mac@macs-MacBook-Pro.local>
2026-03-20 14:26:45 +0530

    ready for render deploy

--- app.py
+++ app.py
@@ -161,5 +161,5 @@ def delete_note(note_id):
 
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(debug=False, host='0.0.0.0', port=port)
